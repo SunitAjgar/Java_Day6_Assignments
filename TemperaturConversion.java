86fabc4b55ace3ff1129a280fae5f1cf2a18ced2
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-11-09 08:27:35 +0530

    Calculated and converted temperature in celsius and farenheit.

--- TemperaturConversion.java
+++ TemperaturConversion.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class TemperaturConversion {
 	
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 		
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the temperature in Farenheit");

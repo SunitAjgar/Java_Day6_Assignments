e347fe8fc08f1ddc86cfda0533183ad31398a087
Sunit Ajgar <109612599+SunitAjgar@users.noreply.github.com>
2022-11-09 08:26:55 +0530

    Calculated and converted the temperature in celsius and farenheit.

---
+++ TemperaturConversion.java
@@ -0,0 +1,23 @@
+package com.blz.day6;
+
+import java.util.Scanner;
+
+public class TemperaturConversion {
+	
+	public static void main(String[] args) {
+		
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the temperature in Farenheit");
+    float faren = sc.nextInt();
+    tempratureConvert(faren);
+	}
+	
+	static void tempratureConvert(float faren) {
+		
+	float Celsius = (faren - 32) * 5/9;
+	System.out.println("The temprature in Celsius is : "+Celsius);
+	faren = (Celsius * 9/5) + 32;
+	System.out.println("The temperature in Farenheit is : "+faren);
+
+	}
+}
